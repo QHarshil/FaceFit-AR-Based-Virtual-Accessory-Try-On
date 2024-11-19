9d1443a3f132e6728d6e487f572f9f899e767048
lqingman <qingmanli@gmail.com>
2024-11-18 22:40:13 -0800

    implement facemesh for camera

--- frontend_web/src/App.js
+++ frontend_web/src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Webcam from "react-webcam";
+import { FaceMesh } from '@mediapipe/face_mesh';
+import * as Facemesh from '@mediapipe/face_mesh';
+import * as cam from '@mediapipe/camera_utils';
+import { useRef, useEffect } from 'react';
+import { drawConnectors } from '@mediapipe/drawing_utils';
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  var camera = null;
+
+  function onResults(results) {
+    const canvasElement = canvasRef.current;
+    const canvasCtx = canvasElement.getContext("2d");
+
+    canvasElement.width = webcamRef.current.video.videoWidth;
+    canvasElement.height = webcamRef.current.video.videoHeight;
+
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+
+    if (results.multiFaceLandmarks) {
+      for (const landmarks of results.multiFaceLandmarks) {
+        drawConnectors(canvasCtx, landmarks, Facemesh.FACEMESH_TESSELATION, { color: '#C0C0C070', lineWidth: 0.5 });
+        drawConnectors(canvasCtx, landmarks, Facemesh.FACEMESH_RIGHT_EYE, { color: '#FF3030', lineWidth: 1 });
+        drawConnectors(canvasCtx, landmarks, Facemesh.FACEMESH_RIGHT_EYEBROW, { color: '#FF3030', lineWidth: 1 });
+        drawConnectors(canvasCtx, landmarks, Facemesh.FACEMESH_LEFT_EYE, { color: '#30FF30', lineWidth: 1 });
+        drawConnectors(canvasCtx, landmarks, Facemesh.FACEMESH_LEFT_EYEBROW, { color: '#30FF30', lineWidth: 1 });
+        drawConnectors(canvasCtx, landmarks, Facemesh.FACEMESH_FACE_OVAL, { color: '#E0E0E0', lineWidth: 1 });
+        drawConnectors(canvasCtx, landmarks, Facemesh.FACEMESH_LIPS, { color: '#E0E0E0', lineWidth: 1 });
+      }
+    }
+    canvasCtx.restore();
+  }
+
+  useEffect(() => {
+    const faceMesh = new FaceMesh({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+      }
+    });
+
+    faceMesh.setOptions({
+      maxNumFaces: 1,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+    });
+
+    faceMesh.onResults(onResults);
+
+    if (typeof webcamRef.current !== "undefined" && webcamRef.current !== null) {
+      camera = new cam.Camera(webcamRef.current.video, {
+        onFrame: async () => {
+          await faceMesh.send({ image: webcamRef.current.video });
+        },
+        width: 1280,
+        height: 720
+      });
+      camera.start();
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Webcam
+        ref={webcamRef}
+        videoConstraints={{
+          width: 1280,
+          height: 720,
+          facingMode: "user"
+        }}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zIndex: 9,
+          width: "100%",
+          height: "auto",
+        }}
+      />
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zIndex: 9,
+          width: "100%",
+          height: "auto",
+        }}
+      ></canvas>
     </div>
   );
 }
