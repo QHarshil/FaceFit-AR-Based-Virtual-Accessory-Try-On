068ccb4b1818938ec85b75c8eabb9697d8ec33e1
xojoyboy <harshil_c@hotmail.com>
2024-12-05 17:27:13 -0800

    updated backend webconfig

--- backend-java/src/main/java/com/arvirtualtryon/config/WebConfig.java
+++ backend-java/src/main/java/com/arvirtualtryon/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.arvirtualtryon.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +13,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/api/resources/**")
+                .addResourceLocations("classpath:/models/");
     }
 }
